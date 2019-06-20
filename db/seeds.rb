7f3fa70f503134f0195e3eaf316920fb9d695a1b
John Lee <jslee1992@gmail.com>
2019-06-20 12:26:30 -0400

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(name: 'john', password: '123')
-User.create(name: 'bob', password: '123')
-User.create(name: 'george', password: '123')
-User.create(name: 'felix', password: '123')
+User.create(name: 'Evans Wang', password: '123', profile_url: 'https://res.cloudinary.com/sungchan/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1561046536/user%20icons/Screen_Shot_2019-06-20_at_12.01.41_PM_s5co4q.png')
+
+User.create(name: 'Alex Griffith', password: '123', profile_url: 'https://res.cloudinary.com/sungchan/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1561046536/user%20icons/Screen_Shot_2019-06-20_at_12.01.52_PM_ftuj6f.png')
+
+User.create(name: 'Vera Popova', password: '123', profile_url: 'https://res.cloudinary.com/sungchan/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1561046536/user%20icons/Screen_Shot_2019-06-20_at_12.01.25_PM_hugzqr.png')
+
+User.create(name: 'Jeff hWang', password: '123', profile_url: 'https://res.cloudinary.com/sungchan/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1561046536/user%20icons/Screen_Shot_2019-06-20_at_12.01.16_PM_ciykxr.png')
+
+User.create(name: 'Chris Metzger', password: '123', profile_url: 'https://res.cloudinary.com/sungchan/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1560998133/Screen_Shot_2019-06-19_at_10.34.48_PM_xua8pq.png')
